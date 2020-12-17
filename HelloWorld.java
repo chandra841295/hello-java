6bbdf36a8871140139625e0929b05212133e45d3
Ubuntu <ubuntu@ip-172-31-28-14.us-east-2.compute.internal>
2020-12-17 01:25:01 +0000

    added data first version

--- HelloWorld.java
+++ HelloWorld.java
@@ -21,6 +21,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello World, DevOps has arrived!" to the terminal window.
         System.out.println("Hello World, DevOps has arrived!");
+	System.out.println("First version");
     }
 
 }

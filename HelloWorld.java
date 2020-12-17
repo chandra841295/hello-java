04ef67016b08f1af0105255c5450515926a520f5
Ubuntu <ubuntu@ip-172-31-28-14.us-east-2.compute.internal>
2020-12-17 01:41:41 +0000

    updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -23,7 +23,7 @@ public class HelloWorld {
 
         System.out.println("Hello World, DevOps has arrived!");
 	System.out.println("First version");
-        System.out.println("Its version 1.2")
+        System.out.println("Its version 1.2");
         System.out.println("Hello World, DevOps has arrived! New Text Added Just Now");
 
     }
